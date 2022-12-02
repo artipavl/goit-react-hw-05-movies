ad47df0031eddc07ff9bf392fce9f9866aebe51d
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 16:20:37 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { getMovieDetails } from 'Api/Api';
+import MovieDetalsInformation from 'components/MovieDetalsInformation/MovieDetalsInformation';
 import { useEffect, useState } from 'react';
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
@@ -8,43 +9,18 @@ const MovieDetails = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    getMovieDetails(movieId).then(data => setData(data));
+    getMovieDetails(movieId)
+      .then(data => setData(data))
+      .catch(console.error);
   }, [movieId]);
 
   if (data.length < 1) {
     return;
   }
 
-  const genres = data.genres.map(genre => genre.name).join(', ');
-
   return (
     <div>
-      <div>
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-          alt={data.original_title}
-          width="150"
-        />
-        <div>
-          <h2>{data.original_title}</h2>
-          <p>User Score: {data.popularity}</p>
-          <b>Overview</b>
-          <p>{data.overview}</p>
-          <b>Ganres</b>
-          <p>{genres}</p>
-        </div>
-      </div>
-      <div>
-        <h3>Additinal information</h3>
-        <ul>
-          <li>
-            <Link to="cast">cast</Link>
-          </li>
-          <li>
-            <Link to="reviews">reviews</Link>
-          </li>
-        </ul>
-      </div>
+      <MovieDetalsInformation data={data} />
       <Outlet />
     </div>
   );
