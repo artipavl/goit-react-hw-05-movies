d5738f7805eef28c6749f242b526c2b0f0a8f379
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 16:21:11 +0200

    add SearchMovieList

---
+++ src/components/SearchMovieList/SearchMovieList.jsx
@@ -0,0 +1,20 @@
+import { Link, useLocation } from "react-router-dom";
+import { List } from "./Style";
+
+const SearchMovieList = ({ data }) => {
+    const location = useLocation();
+
+    return (
+      <List>
+        {data.map(movie => (
+          <li key={movie.id}>
+            <Link to={`${movie.id}`} state={location}>
+              {movie.original_title}
+            </Link>
+          </li>
+        ))}
+      </List>
+    );
+}
+
+export default SearchMovieList
