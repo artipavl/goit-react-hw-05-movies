94fd3db195f4b6571f4879f95fec4570dce824c4
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 02:06:39 +0200

    Create Reviews.jsx

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,34 @@
+import { getMovieReviews } from 'Api/Api';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const Reviews = () => {
+  const { movieId } = useParams();
+
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    getMovieReviews(movieId, 1).then(data => setReviews(data.results));
+  }, [movieId]);
+
+  if (reviews.length < 1) {
+      return (<div>
+        <p>Pusto</p>
+    </div>)
+  }
+
+  return (
+    <div>
+      <ul>
+        {reviews.map(review => (
+          <li key={review.id}>
+            <b>Auther: {review.author}</b>
+            <p>{review.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Reviews;
