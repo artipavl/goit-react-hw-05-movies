bf82ab2520e58c610b9f26d979bd970781f7cbc7
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 16:21:15 +0200

    Update SearchMovie.jsx

--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -1,37 +1,30 @@
-import { getSearchMovie } from 'Api/Api';
-import { useEffect } from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 const SearchMovie = () => {
-  const [data, setData] = useState([]);
   const [search, setSearch] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchUrl = searchParams.get('q');
 
-  useEffect(() => {
-    if (search) {
-      getSearchMovie(search, 1).then(data => setData(data));
+  function onSubmit(e) {
+    e.preventDefault();
+    if (e.currentTarget.elements.search.value === '') {
+      return;
     }
-  }, [search]);
+    setSearchParams({ q: e.currentTarget.elements.search.value });
+    setSearch('');
+  }
 
   return (
-    <div>
-      <h1>Trending</h1>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
-        value={search}
+        name="search"
+        value={search ?? searchUrl}
         onChange={e => setSearch(e.target.value)}
       />
-      {search && (
-        <ul>
-          {data.length > 0 &&
-            data.map(movie => (
-              <li key={movie.id}>
-                <Link to={`${movie.id}`}>{movie.original_title}</Link>
-              </li>
-            ))}
-        </ul>
-      )}
-    </div>
+      <button type="submit">Search</button>
+    </form>
   );
 };
 
