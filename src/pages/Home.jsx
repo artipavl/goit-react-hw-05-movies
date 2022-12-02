775bf9259c7041b98302d848dcf9ef870f0d6152
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 16:20:41 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
-import Trending from 'components/Trending/Trending';
+import { getTrending } from 'Api/Api';
+import TrendingList from 'components/TrendingList/TrendingList';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Home = () => {
-  return (
-    <div>
-      <p>Home</p>
-        <Trending />
-    </div>
-  );
+  const [data, setData] = useState([]);
+  
+ useEffect(() => {
+   getTrending().then(data => setData(data)).catch(console.error);
+ }, []);
+
+ return (
+   <div>
+     <h1>Trending todey</h1>
+     <TrendingList data={data} />
+   </div>
+ );
 };
 
 export default Home;
