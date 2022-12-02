95d89f0031df7e416d2926379245239013b23c59
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 16:21:46 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,22 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MovieDetails from 'pages/MovieDetails';
 import Cast from './Cast/Cast';
 import Reviews from './Reviews/Reviews';
+import Header from './Header/Header';
 
 export function App() {
-  // useEffect(() => {
-  //   getTrending();
-  // }, []);
 
   return (
     <div className="App">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="movies">Movies</Link>
-      </nav>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </div>
