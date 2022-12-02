511e3c4184eb6a74bf2f234644f8e17ce1742dc3
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 16:20:49 +0200

    Create TrendingList.jsx

---
+++ src/components/TrendingList/TrendingList.jsx
@@ -0,0 +1,20 @@
+import { Link, useLocation } from 'react-router-dom';
+import { List } from './Style';
+
+const TrendingList = ({data}) => {
+  const location = useLocation();
+
+  return (
+    <List>
+      {data.map(movie => (
+        <li key={movie.id}>
+          <Link to={`movies/${movie.id}`} state={location}>
+            {movie.original_title || movie.name}
+          </Link>
+        </li>
+      ))}
+    </List>
+  );
+};
+
+export default TrendingList;
