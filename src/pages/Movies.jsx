8403360a0850dd9a2924e1d4f382ba3ad8ffa73f
artipavl <40296816+artipavl@users.noreply.github.com>
2022-12-02 16:20:31 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,10 +1,31 @@
+import { getSearchMovie } from 'Api/Api';
 import SearchMovie from 'components/SearchMovie/SearchMovie';
+import SearchMovieList from 'components/SearchMovieList/SearchMovieList';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
+  const [data, setData] = useState([]);
+
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('q');
+
+
+  useEffect(() => {
+    if (search) {
+      getSearchMovie(search, 1)
+        .then(data => setData(data))
+        .catch(console.error);
+    }
+  }, [search]);
+
   return (
     <div>
-      <p>Movies</p>
-      <SearchMovie/>
+      <SearchMovie />
+      {data.length > 0 && (
+        <SearchMovieList data={data} />
+      )}
     </div>
   );
 };
